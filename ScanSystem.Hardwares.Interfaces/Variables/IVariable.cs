cecc4df1e17e06ec9f70df3a11e722836e57c4c4
valitov.gm <valitov.gm@eka.ru>
2019-11-29 17:51:31 +0300

    1

--- ScanSystem.Hardwares.Interfaces/Variables/IVariable.cs
+++ ScanSystem.Hardwares.Interfaces/Variables/IVariable.cs
@@ -5,9 +5,10 @@ using System.Text;
 namespace ScanSystem.Hardwares.Interfaces.Variables
 {
     public interface IVariable<TType>
+        where TType : IComparable, IComparable<TType>, IConvertible, IEquatable<TType>
     {
         string Name { get; set; }
-        IAddress Address { get; set; }
+        IAddress Address { get; }
         TType Value { get; set; }
     }
 }
