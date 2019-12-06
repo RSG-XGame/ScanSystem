b58dcd4d0e7e3db37200b8a679301a1b8466b370
Cholak.AD <Cholak.AD@eka.ru>
2019-12-06 18:18:40 +0300

    test

---
+++ ScanApp/ClientApp/Models/login.js
@@ -0,0 +1,9 @@
+var Product = /** @class */ (function () {
+    function Product(userName, company) {
+        this.userName = userName;
+        this.company = company;
+    }
+    return Product;
+}());
+export { Product };
+//# sourceMappingURL=login.js.map
---
+++ ScanApp/ClientApp/Models/login.ts
@@ -0,0 +1,5 @@
+﻿export class Product {
+    constructor(
+        public userName?: string,
+        public company?: string) { }
+}
