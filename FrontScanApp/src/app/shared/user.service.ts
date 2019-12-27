b0058122c21548ae0b4453f0a6dfc953dbabf306
Cholak.AD <Cholak.AD@eka.ru>
2019-12-27 18:41:15 +0300

    Commit New Front

---
+++ FrontScanApp/src/app/shared/user.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { FormBuilder, Validators, FormGroup, AbstractControl } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private fb: FormBuilder, private http: HttpClient) { }
+  readonly BaseURI = 'http://localhost:54277/api';
+
+  formModel = this.fb.group({
+    UserName: ['', Validators.required],
+    Email: ['', Validators.email],
+    FullName: [''],
+    Passwords: this.fb.group({
+      Password: ['', [Validators.required, Validators.minLength(6)]],
+      ConfirmPassword: ['', Validators.required]
+    }, { validator: this.comparePasswords })
+
+  });
+
+  comparePasswords(fb: FormGroup) {
+    const confirmPswrdCtrl = fb.get('ConfirmPassword');
+    // passwordMismatch
+    // confirmPswrdCtrl.errors={passwordMismatch:true}
+    if (confirmPswrdCtrl != null) {
+      if (confirmPswrdCtrl.errors == null || 'passwordMismatch' in confirmPswrdCtrl.errors) {
+        const pswd = fb.get('Password');
+        if (pswd != null && pswd.value !== confirmPswrdCtrl.value) {
+            confirmPswrdCtrl.setErrors({ passwordMismatch: true });
+          } else {
+          confirmPswrdCtrl.setErrors(null);
+        }
+      }
+    }
+  }
+
+  register() {
+    const body = {
+      UserName: this.formModel.value.UserName,
+      Email: this.formModel.value.Email,
+      FullName: this.formModel.value.FullName,
+      Password: this.formModel.value.Passwords.Password
+    };
+    return this.http.post(this.BaseURI + '/ApplicationUser/Register', body);
+  }
+
+  login(formData: any) {
+    return this.http.post(this.BaseURI + '/ApplicationUser/Login', formData);
+  }
+
+  getUserProfile() {
+    return this.http.get(this.BaseURI + '/UserProfile');
+  }
+}
