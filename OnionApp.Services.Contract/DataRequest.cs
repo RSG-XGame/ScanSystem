6ffa99cd62baf4481089841cc89c55124694eabf
valitov.gm <valitov.gm@eka.ru>
2019-12-30 13:25:58 +0300

    1

---
+++ OnionApp.Services.Contract/DataRequest.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using OnionApp.Services.Contract.ContractModels;
+using ScanSystem.Base.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnionApp.Services.Contract
+{
+    public class DataRequest : IRequest
+    {
+        [JsonConverter(typeof(StringEnumConverter))]
+        public MainAPITypes APIType { get; set; }
+        public dynamic Data { get; set; }
+    }
+
+    public class DataRequest<TType> : DataRequest
+    {
+        [JsonIgnore]
+        public TType Source => (TType)Data;
+    }
+}
